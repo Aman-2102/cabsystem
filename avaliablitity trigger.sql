0fd114834bfdd98280bb554dac54cf0c26882ad4
Aman-2102 <111444745+Aman-2102@users.noreply.github.com>
2023-08-28 14:51:32 +0530

    Delete avaliablitity trigger.sql

--- avaliablitity trigger.sql
+++
@@ -1,9 +0,0 @@
-create trigger updatedriver
-on bookingdetail
-AFTER INSERT
-AS
-BEGIN
-update DriverLocation set Avaliablity='no' 
- WHERE   DriverLocation.Avaliablity ='yes' and DriverId in (select inserted.driverid from inserted)
- end
-
